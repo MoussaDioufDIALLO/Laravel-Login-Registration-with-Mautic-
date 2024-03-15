c6a488353a0fa6f9de193105726bdc2648bf92cd
mxzvrt <moussadioufdiallo@gmail.com>
2024-03-15 11:34:54 +0000

    '15/03/2024-11H35'

---
+++ CustomLoginRegister/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginRegisterController ; 
+ 
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['middleware' => 'web'], function () {
+    Route::get('/register', [LoginRegisterController::class, 'register'])->name('register');
+    Route::post('/store', [LoginRegisterController::class, 'store'])->name('store');
+    Route::get('/login', [LoginRegisterController::class, 'login'])->name('login');
+    Route::post('/authenticate', [LoginRegisterController::class, 'authenticate'])->name('authenticate');
+    Route::get('/dashboard', [LoginRegisterController::class, 'dashboard'])->name('dashboard');
+    Route::post('/logout', [LoginRegisterController::class, 'logout'])->name('logout');
+});
